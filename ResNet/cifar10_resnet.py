c0eeb18e478e22403931bd946fda6fa50e2a05f8
losstie <losstie@outlook.com>
2019-05-09 10:46:54 +0800

    "update"

--- ResNet/cifar10_resnet.py
+++ ResNet/cifar10_resnet.py
@@ -7,12 +7,13 @@
 @create_time: 2019/5/8 14:23
 @description:
 """
+
 from __future__ import absolute_import
 from __future__ import  division
 from __future__ import print_function
 
 import os
-
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 from absl import app as absl_app
 from absl import flags
 import tensorflow as tf
@@ -30,8 +31,8 @@ NUM_CLASSES = 10
 _NUM_DATA_FILES = 5
 
 NUM_IMAGES = {
-    'train': 48000,
-    'validation': 12000,
+    'train': 50000,
+    'validation': 10000,
 }
 
 DATASET_NAME = 'CIFAR-10'
@@ -46,30 +47,36 @@ def get_filenames(is_training, data_dir):
         'Run cifar-10_download_and_extract.py first to download and extract the'
         'CIFAR-10 data.')
     if is_training:
-        return [os.path.join(data_dir,'data_batch_%d.bin' % i) for i in range(1, _NUM_DATA_FILES + 1)]
+
+        return [os.path.join(data_dir,'train.tfrecords')]
     else:
-        return [os.path.join(data_dir, 'test_batch.bin')]
+        return [os.path.join(data_dir, 'eval.tfrecords')]
 
 
 def parse_record(raw_record, is_training, dtype):
     """Parse CIFAR-10 image and label from a raw record."""
+    keys_to_features={
+        'image_raw':tf.FixedLenFeature((), tf.string),
+        'label': tf.FixedLenFeature((), tf.int64)
+    }
+    parsed = tf.parse_single_example(raw_record, keys_to_features)
     # convert bytes to a vector of uint8 that is _RECORD_BYTES long
-    record_vector = tf.io.decode_raw(raw_record, tf.uint8)
+    image = tf.decode_raw(parsed['image_raw'], tf.uint8)
+    image = tf.cast(image, tf.float32)
 
-    # The first byte represents the label, which we convert from uint8 to int32
+    # which we convert from uint8 to int32
     # and then to one-hot
-    label = tf.cast(record_vector[0], tf.int32)
+    label = tf.cast(parsed['label'], tf.int32)
 
     # The remaining bytes after the label represent the image, which we reshape
     # from [depth * height * width] to [depth, height, width].
 
-    depth_major = tf.reshape(record_vector[1:_RECORD_BYTES], [NUM_CHANNELS, HEIGHT, WIDTH])
-
+    image = tf.reshape(image, [NUM_CHANNELS, HEIGHT, WIDTH])
+    print(image.shape)
     # Convert from [depth, height, width] to [height, width, depth], and cast as
     # float32.
 
-    image = tf.cast(tf.transpose(a=depth_major, perm=[1, 2, 0]), tf.float32)
-
+    image = tf.cast(tf.transpose(a=image, perm=[1, 2, 0]), tf.float32)
     image = preprocess_image(image, is_training)
     image = tf.cast(image, dtype)
 
@@ -117,10 +124,10 @@ def input_fn(is_training, data_dir, batch_size, shuffle_buffer=NUM_IMAGES['train
         A dataset that can be used for iteration.
     """
     filenames = get_filenames(is_training, data_dir)
-    dataset = tf.data.FixedLengthRecordDataset(filenames, _RECORD_BYTES)
-
+    # dataset = tf.data.FixedLengthRecordDataset(filenames, _RECORD_BYTES)
+    dataset = tf.data.TFRecordDataset(filenames=filenames)
     options = tf.data.Options()
-    options.experimental_threading.max_intra_op_parallelism = 1
+    # options.experimental_threading.max_intra_op_parallelism = 1
     dataset = dataset.with_options(options)
 
     # Prefetches a batch at a time to smooth out the time taken to load input
@@ -144,7 +151,101 @@ def input_fn(is_training, data_dir, batch_size, shuffle_buffer=NUM_IMAGES['train
     # allows DistributionStrategies to adjust how many batches to fetch based
     # on how many devices are present.
     dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    # train_iterator = dataset.make_one_shot_iterator()
+
+    # features, labels = train_iterator.get_next()
     return dataset
+
+
+############################################################################
+# utils
+############################################################################
+def learning_rate_with_decay(batch_size, batch_denom, num_images, boundary_epochs,
+                             decay_rates, base_lr=0.1, warmup=False):
+    """Get a learning rate that decays steps-wise as training progress
+    Args:
+        batch_size:
+        batch_denom:
+        num_images:
+        boundary_epochs:
+        decay_rates:
+        base_lr:
+        warmup:
+    Return:
+        Returns a function that takes a single argument - the number of batches
+    trained so far (global_step)- and returns the learning rate to be used
+    for training the next batch.
+    """
+    initial_learning_rate = base_lr * batch_size / batch_denom
+    batches_per_epoch = num_images / batch_size
+
+    # Reduce the learning rate at certain epochs.
+    # CIFAR-10: divide by 10 at epoch 100, 150, and 200
+    # ImageNet: divide by 10 at epoch 30, 60, 80, and 90
+    boundaries = [int(batches_per_epoch * epoch) for epoch in boundary_epochs]
+    vals = [initial_learning_rate * decay for decay in decay_rates]
+
+    def learning_rate_fn(global_step):
+        """Builds scaled learning rate function with 5 epoch warm up."""
+        lr = tf.compat.v1.train.piecewise_constant(global_step, boundaries, vals)
+        if warmup:
+            warmup_steps = int(batches_per_epoch * 5)
+            warmup_lr = (
+                    initial_learning_rate * tf.cast(global_step, tf.float32) / tf.cast(
+                warmup_steps, tf.float32))
+            return tf.cond(pred=global_step < warmup_steps,
+                           true_fn=lambda: warmup_lr,
+                           false_fn=lambda: lr)
+        return lr
+
+    def poly_rate_fn(global_step):
+        """Handles linear scaling rule, gradual warmup, and LR decay.
+        The learning rate starts at 0, then it increases linearly per step.  After
+        FLAGS.poly_warmup_epochs, we reach the base learning rate (scaled to account
+        for batch size). The learning rate is then decayed using a polynomial rate
+        decay schedule with power 2.0.
+        Args:
+          global_step: the current global_step
+        Returns:
+          returns the current learning rate
+        """
+
+        # Learning rate schedule for LARS polynomial schedule
+        if flags.FLAGS.batch_size < 8192:
+            plr = 5.0
+            w_epochs = 5
+        elif flags.FLAGS.batch_size < 16384:
+            plr = 10.0
+            w_epochs = 5
+        elif flags.FLAGS.batch_size < 32768:
+            plr = 25.0
+            w_epochs = 5
+        else:
+            plr = 32.0
+            w_epochs = 14
+
+        w_steps = int(w_epochs * batches_per_epoch)
+        wrate = (plr * tf.cast(global_step, tf.float32) / tf.cast(
+            w_steps, tf.float32))
+
+        # TODO(pkanwar): use a flag to help calc num_epochs.
+        num_epochs = 90
+        train_steps = batches_per_epoch * num_epochs
+
+        min_step = tf.constant(1, dtype=tf.int64)
+        decay_steps = tf.maximum(min_step, tf.subtract(global_step, w_steps))
+        poly_rate = tf.train.polynomial_decay(
+            plr,
+            decay_steps,
+            train_steps - w_steps + 1,
+            power=2.0)
+        return tf.where(global_step <= w_steps, wrate, poly_rate)
+
+    # For LARS we have a new learning rate schedule
+    if flags.FLAGS.enable_lars:
+        return poly_rate_fn
+
+    return learning_rate_fn
 
 
 ###############################################################################
@@ -194,21 +295,47 @@ class Cifar10Model(resnet_model.Model):
 def cifar10_model_fn(features, labels, mode, params):
     """Model function for resnet"""
     features = tf.reshape(features, [-1, HEIGHT, WIDTH, NUM_CHANNELS])
-    return resnet_model_fn(features=features,
-                           labels=labels,
-                           mode=mode,
-                           model_class=Cifar10Model,
-                           resnet_size=params['resnet_size'],
-                           momentum=0.9,
-                           data_format=params['data_format'],
-                           resnet_version=params['resnet_version'],
-                           dtype=params['dtype'],
-                           fine_tune=params['fine_tune'])
+    # Learning rate schedule follows arXiv:1512.03385 for ResNet-56 and under.
+    learning_rate_fn = learning_rate_with_decay(
+        batch_size=params['batch_size'] * params.get('num_workers', 1),
+        batch_denom=128, num_images=NUM_IMAGES['train'],
+        boundary_epochs=[91, 136, 182], decay_rates=[1, 0.1, 0.01, 0.001])
+
+    # Weight decay of 2e-4 diverges from 1e-4 decay used in the ResNet paper
+    # and seems more stable in testing. The difference was nominal for ResNet-56.
+    weight_decay = 2e-4
+
+    # Empirical testing showed that including batch_normalization variables
+    # in the calculation of regularized loss helped validation accuracy
+    # for the CIFAR-10 dataset, perhaps because the regularization prevents
+    # overfitting on the small data set. We therefore include all vars when
+    # regularizing and computing loss during training.
+    def loss_filter_fn(_):
+        return True
+
+    return resnet_model_fn(
+        features=features,
+        labels=labels,
+        mode=mode,
+        model_class=Cifar10Model,
+        resnet_size=params['resnet_size'],
+        weight_decay=weight_decay,
+        learning_rate_fn=learning_rate_fn,
+        momentum=0.9,
+        data_format=params['data_format'],
+        resnet_version=params['resnet_version'],
+        loss_scale=params['loss_scale'],
+        loss_filter_fn=loss_filter_fn,
+        dtype=params['dtype'],
+        fine_tune=params['fine_tune']
+    )
 
 
 def resnet_model_fn(features, labels, mode, model_class, resnet_size,
-                    momentum, data_format,resnet_version, dtype=resnet_model.DEFAULT_DTYPE,
-                    fine_tune=False):
+                    weight_decay, learning_rate_fn, momentum,
+                    data_format,resnet_version, loss_scale,
+                    loss_filter_fn=None, dtype=resnet_model.DEFAULT_DTYPE,
+                    fine_tune=False, label_smoothing=0.0):
     model = model_class(resnet_size, data_format, resnet_version=resnet_version, dtype=dtype)
 
     logits = model(features, mode == tf.estimator.ModeKeys.TRAIN)
@@ -224,13 +351,94 @@ def resnet_model_fn(features, labels, mode, model_class, resnet_size,
         return tf.estimator.EstimatorSpec(
             mode=mode,
             predictions=predictions)
-    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels,logits=logits)
+
+    # Calculate loss, which includes softmax cross entropy and L2 regularization.
+    if label_smoothing != 0.0:
+        one_hot_labels = tf.one_hot(labels, 1001)
+        cross_entropy = tf.losses.softmax_cross_entropy(
+            logits=logits, onehot_labels=one_hot_labels,
+            label_smoothing=label_smoothing)
+    else:
+        cross_entropy = tf.compat.v1.losses.sparse_softmax_cross_entropy(
+            logits=logits, labels=labels)
+
+        # Create a tensor named cross_entropy for logging purposes.
+    tf.identity(cross_entropy, name='cross_entropy')
+    tf.compat.v1.summary.scalar('cross_entropy', cross_entropy)
+
+    # If no loss_filter_fn is passed, assume we want the default behavior,
+    # which is that batch_normalization variables are excluded from loss.
+    def exclude_batch_norm(name):
+        return 'batch_normalization' not in name
+
+    loss_filter_fn = loss_filter_fn or exclude_batch_norm
+
+    # Add weight decay to the loss.
+    l2_loss = weight_decay * tf.add_n(
+        # loss is computed using fp32 for numerical stability.
+        [
+            tf.nn.l2_loss(tf.cast(v, tf.float32))
+            for v in tf.compat.v1.trainable_variables()
+            if loss_filter_fn(v.name)
+        ])
+    tf.compat.v1.summary.scalar('l2_loss', l2_loss)
+    loss = cross_entropy + l2_loss
 
     if mode == tf.estimator.ModeKeys.TRAIN:
-        global_step = tf.train.get_or_create_global_step()
-        optimizr = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-        train_op = optimizr.minimize(loss=loss, global_step=global_step)
-        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+        global_step = tf.compat.v1.train.get_or_create_global_step()
+
+        learning_rate = learning_rate_fn(global_step)
+
+        # Create a tensor named learning_rate for logging purposes
+        tf.identity(learning_rate, name='learning_rate')
+        tf.compat.v1.summary.scalar('learning_rate', learning_rate)
+
+        if flags.FLAGS.enable_lars:
+            optimizer = tf.contrib.opt.LARSOptimizer(
+                learning_rate,
+                momentum=momentum,
+                weight_decay=weight_decay,
+                skip_list=['batch_normalization', 'bias'])
+        else:
+            optimizer = tf.compat.v1.train.MomentumOptimizer(
+                learning_rate=learning_rate,
+                momentum=momentum
+            )
+
+        def _dense_grad_filter(gvs):
+            """Only apply gradient updates to the final layer.
+            This function is used for fine tuning.
+            Args:
+              gvs: list of tuples with gradients and variable info
+            Returns:
+              filtered gradients so that only the dense layer remains
+            """
+            return [(g, v) for g, v in gvs if 'dense' in v.name]
+
+        if loss_scale != 1:
+            # When computing fp16 gradients, often intermediate tensor values are
+            # so small, they underflow to 0. To avoid this, we multiply the loss by
+            # loss_scale to make these tensor values loss_scale times bigger.
+            scaled_grad_vars = optimizer.compute_gradients(loss * loss_scale)
+
+            if fine_tune:
+                scaled_grad_vars = _dense_grad_filter(scaled_grad_vars)
+
+            # Once the gradient computation is complete we can scale the gradients
+            # back to the correct scale before passing them to the optimizer.
+            unscaled_grad_vars = [(grad / loss_scale, var)
+                                  for grad, var in scaled_grad_vars]
+            minimize_op = optimizer.apply_gradients(unscaled_grad_vars, global_step)
+        else:
+            grad_vars = optimizer.compute_gradients(loss)
+            if fine_tune:
+                grad_vars = _dense_grad_filter(grad_vars)
+            minimize_op = optimizer.apply_gradients(grad_vars, global_step)
+
+        update_ops = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.UPDATE_OPS)
+        train_op = tf.group(minimize_op, update_ops)
+    else:
+        train_op = None
 
     accuracy = tf.compat.v1.metrics.accuracy(labels, predictions['classes'])
     accuracy_top_5 = tf.compat.v1.metrics.mean(
@@ -246,15 +454,36 @@ def resnet_model_fn(features, labels, mode, model_class, resnet_size,
 
     return tf.estimator.EstimatorSpec(
         mode=mode,
-        # predictions=predictions,
+        predictions=predictions,
         loss=loss,
+        train_op=train_op,
         eval_metric_ops=metrics)
 
 
 def resnet_main(flags_obj, model_function, input_function, dataset_name, shape=None):
     """"""
+    # Creates session config. allow_soft_placement = True, is required for
+    # multi-GPU and is not harmful for other modes.
+    session_config = tf.compat.v1.ConfigProto(
+        allow_soft_placement=True)
+    # Creates a `RunConfig` that checkpoints every 24 hours which essentially
+    # results in checkpoints determined only by `epochs_between_evals`.
+    run_config = tf.estimator.RunConfig(
+        session_config=session_config,
+        save_checkpoints_secs=60 * 60 * 24,
+        save_checkpoints_steps=None)
+
+    if flags_obj.pretrained_model_checkpoint_path is not None:
+        warm_start_settings = tf.estimator.WarmStartSettings(
+            flags_obj.pretrained_model_checkpoint_path,
+            vars_to_warm_start='^(?!.*dense)')
+    else:
+        warm_start_settings = None
+
+
     classifier = tf.estimator.Estimator(
-        model_fn=model_function, model_dir=flags_obj.model_dir, params={
+        model_fn=model_function, model_dir=flags_obj.model_dir, config=run_config,
+        warm_start_from=warm_start_settings, params={
             'resnet_size': int(flags_obj.resnet_size),
             'data_format': flags_obj.data_format,
             'batch_size': flags_obj.batch_size,
@@ -275,7 +504,9 @@ def resnet_main(flags_obj, model_function, input_function, dataset_name, shape=N
     logging_hook = tf.train.LoggingTensorHook(
         tensors=tensor_to_log, every_n_iter=50
     )
-    classifier.train(input_fn=lambda :input_fn_train(20), steps=1000, hooks=[logging_hook])
+    train_epochs = (0 if flags_obj.mode != 'train' else flags_obj.train_epochs)
+    max_steps = flags_obj.train_counts // flags_obj.batch_size
+    classifier.train(input_fn=lambda :input_fn_train(train_epochs), steps=max_steps, hooks=[logging_hook])
 
     def input_fn_eval():
         return input_function(is_training=False,
@@ -324,6 +555,8 @@ def define_flags(resnet_size_choices=None):
                              "be exported to this directory at the end of training")
     flags.DEFINE_integer(name="batch_size", short_name="bs", default=16,
                          help="Batch size for training and evaluation.")
+    flags.DEFINE_integer(name="train_counts",short_name='tc', default=50000,
+                         help="train_counts for training")
 
     flags.DEFINE_enum(name="resnet_version", short_name="rv",
                       default="1", enum_values=["1", "2"],
@@ -360,8 +593,10 @@ def define_flags(resnet_size_choices=None):
             flags.FLAGS.set_default(name=key, value=value)
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    models_path = os.path.join(dir_path, 'models/')
-    set_default(data_dir='../dataset/cifar-10/',
+    models_path = os.path.join(dir_path, 'models')
+    data_dir = os.path.join(dir_path, 'dataset\cifar-10')
+    print(data_dir)
+    set_default(data_dir='../dataset/cifar-10',
                 model_dir=models_path,
                 mode="train",
                 resnet_size='56',
@@ -373,3 +608,4 @@ if __name__ == "__main__":
     tf.logging.set_verbosity(tf.logging.INFO)
     define_flags()
     tf.app.run()
+
